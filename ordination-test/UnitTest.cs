cbfef2b41f77ac87774436cd909e8e241bf06e3c
rasmussig <rasmus2001@live.dk>
2024-04-27 15:23:40 +0200

    UnitTest af DagligFast

--- ordination-test/UnitTest.cs
+++ ordination-test/UnitTest.cs
@@ -10,9 +10,64 @@ using shared.Model;
 
 public class UnitTest
 {
+    // Test af DagligFast.doegnDosis
+    [TestMethod]
+    public void DagligFast_doegnDosis_TC1()
+    {
+        // Initialisere DagligFast objekt
+        var dagligFast = new DagligFast(new DateTime(2023, 1, 1), new DateTime(2023, 1, 1), null, 10, 10, 10, 10);
+
+        // Beregner daglis dosis, med metoden doegnDosis    
+        double result = dagligFast.doegnDosis();
+
+        // Kontroller output stemmer overens med forventede værdi
+        Assert.AreEqual(40, result);
+    }
 
     [TestMethod]
+    public void DagligFast_doegnDosis_TC2()
+    {
+        // Initialisere DagligFast objekt
+        var dagligFast = new DagligFast(new DateTime(2023, 1, 1), new DateTime(2023, 1, 1), null, 5, 8, 5, 0);
 
+        // Beregner daglis dosis, med metoden doegnDosis    
+        double result = dagligFast.doegnDosis();
+
+        // Kontroller output stemmer overens med forventede værdi
+        Assert.AreEqual(18, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void DagligFast_doegnDosis_TC3()
+    {
+        // Initialisere DagligFast objekt
+        var dagligFast = new DagligFast(new DateTime(2023, 1, 1), new DateTime(2023, 1, 1), null, 0, 0, 0, 0);
+
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void DagligFast_doegnDosis_TC4()
+    {
+        // Initialisere DagligFast objekt
+        var dagligFast = new DagligFast(new DateTime(2023, 1, 1), new DateTime(2023, 1, 1), null, -5, 0, 5, 5);
+
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void DagligFast_doegnDosis_TC5()
+    {
+        // Initialisere DagligFast objekt
+        var dagligFast = new DagligFast(new DateTime(2023, 1, 1), new DateTime(2023, 1, 1), null, -5, -5, -5, 5);
+    }
+
+    // Test af DagligSkæv.doegnDosis
+    // TODO
+
+    // Test af PN.doegnDosis
+    [TestMethod]
     public void PN_doegnDosis_TC1()
     {
         // Arrange
--- shared/Model/DagligFast.cs
+++ shared/Model/DagligFast.cs
@@ -3,39 +3,53 @@ namespace shared.Model;
 using System.Xml.Schema;
 using static shared.Util;
 
-public class DagligFast : Ordination {
-	
-	public Dosis MorgenDosis { get; set; } = new Dosis();
-    public Dosis MiddagDosis { get; set; } = new Dosis();
-    public Dosis AftenDosis { get; set; } = new Dosis();
-    public Dosis NatDosis { get; set; } = new Dosis();
+public class DagligFast : Ordination
+{
 
-	public DagligFast(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel, double morgenAntal, double middagAntal, double aftenAntal, double natAntal) : base(laegemiddel, startDen, slutDen) {
-        MorgenDosis = new Dosis(CreateTimeOnly(6, 0, 0), morgenAntal);
-        MiddagDosis = new Dosis(CreateTimeOnly(12, 0, 0), middagAntal);
-        AftenDosis = new Dosis(CreateTimeOnly(18, 0, 0), aftenAntal);
-        NatDosis = new Dosis(CreateTimeOnly(23, 59, 0), natAntal);
+	public Dosis MorgenDosis { get; set; } = new Dosis();
+	public Dosis MiddagDosis { get; set; } = new Dosis();
+	public Dosis AftenDosis { get; set; } = new Dosis();
+	public Dosis NatDosis { get; set; } = new Dosis();
+
+	public DagligFast(DateTime startDen, DateTime slutDen, Laegemiddel laegemiddel, double morgenAntal, double middagAntal, double aftenAntal, double natAntal) : base(laegemiddel, startDen, slutDen)
+	{
+		// Tjekker om dosis værdier er negative
+		if (morgenAntal < 0 || middagAntal < 0 || aftenAntal < 0 || natAntal < 0)
+			throw new ArgumentException("Dosis værdier må ikke være negative.");
+		// Tjekker om mindst en dosis er større end 0
+		if (morgenAntal == 0 && middagAntal == 0 && aftenAntal == 0 && natAntal == 0)
+			throw new ArgumentException("Mindst en dosis skal være større end 0.");
+
+		MorgenDosis = new Dosis(CreateTimeOnly(6, 0, 0), morgenAntal);
+		MiddagDosis = new Dosis(CreateTimeOnly(12, 0, 0), middagAntal);
+		AftenDosis = new Dosis(CreateTimeOnly(18, 0, 0), aftenAntal);
+		NatDosis = new Dosis(CreateTimeOnly(23, 59, 0), natAntal);
 	}
 
-    public DagligFast() : base(null!, new DateTime(), new DateTime()) {
-    }
+	public DagligFast() : base(null!, new DateTime(), new DateTime())
+	{
+	}
 
-	public override double samletDosis() {
-		
+	public override double samletDosis()
+	{
+
 		return base.antalDage() * doegnDosis();
 	}
 
-	public override double doegnDosis() {
+	public override double doegnDosis()
+	{
 		// TODO: Implement!
-		return MorgenDosis.antal + MiddagDosis.antal + AftenDosis.antal + NatDosis.antal;	
+		return MorgenDosis.antal + MiddagDosis.antal + AftenDosis.antal + NatDosis.antal;
 	}
-	
-	public Dosis[] getDoser() {
-		Dosis[] doser = {MorgenDosis, MiddagDosis, AftenDosis, NatDosis};
+
+	public Dosis[] getDoser()
+	{
+		Dosis[] doser = { MorgenDosis, MiddagDosis, AftenDosis, NatDosis };
 		return doser;
 	}
 
-	public override String getType() {
+	public override String getType()
+	{
 		return "DagligFast";
 	}
 }
